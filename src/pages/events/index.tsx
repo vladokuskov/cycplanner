b73dde19ae5ba0ccc91f61455d0bfbb4eae3c8fe
Swappnet <advance11ua@gmail.com>
2023-03-02 16:28:36 +0200

    added nav, footer, navmenu, favicon, sass folder

---
+++ src/pages/events/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Events() {
+  return (
+    <>
+      <Head>
+        <title>cycplanner - Events</title>
+      </Head>
+      <main className="events-wrapper"></main>
+    </>
+  );
+}
