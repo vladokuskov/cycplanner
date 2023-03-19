3d0250815fd17bd414d340b2d0d61f4dee9f8af4
Swappnet <advance11ua@gmail.com>
2023-03-19 11:54:16 +0200

    added new variant for btn

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,17 +1,18 @@
 interface Button {
   size?: 'sm' | 'md' | 'xl' | 'xxl';
-  type: 'filled' | 'outline' | 'icon' | 'text';
+  type: 'filled' | 'outline' | 'icon' | 'text' | 'icon-outlined';
   label?: string;
   icon?: React.ReactNode;
+  stretched?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ onClick, label, type, icon, size }: Button) => {
-  const buttonClassName = `btn btn-${type} ${size}`;
+const Button = ({ onClick, label, type, icon, size, stretched }: Button) => {
+  const buttonClassName = `btn btn-${type} ${size} ${stretched && 'full'}`;
 
   return (
     <button className={buttonClassName} onClick={onClick} title={label}>
-      {type === 'icon' ? icon : label}
+      {type === 'icon' || type === 'icon-outlined' ? icon : label}
     </button>
   );
 };
