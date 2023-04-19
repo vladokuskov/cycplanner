1ce1e4dcadd545d6637b6119934572ad79a20e9b
Swappnet <advance11ua@gmail.com>
2023-04-19 18:35:10 +0300

    fixed sidebar

--- src/components/styledComponents/profile/ProfileSidebar.tsx
+++ src/components/styledComponents/profile/ProfileSidebar.tsx
@@ -7,11 +7,11 @@ import { ProfilePreview } from '../ProfilePreview';
 
 const SidebarWrapper = styled.div`
   width: 100%;
-  height: 100%;
   padding: 1rem;
   border-bottom: 0.05rem solid #d3d3d3;
   @media (min-width: 680px) {
     border-bottom: none;
+    height: 100%;
   }
 `;
 
