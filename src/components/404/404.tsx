abf469780ec4f47076a9752fc9e9106794aab2ea
Swappnet <advance11ua@gmail.com>
2023-05-01 22:11:15 +0300

    updated 404 page

--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { Title, Description, LayoutWrapper } from './404.styles';
+
+import { Description, LayoutWrapper, Title } from './404.styles';
 
 const FourOFour = () => {
   return (
     <LayoutWrapper>
+      <Image src="/assets/logo.svg" alt="" width={200} height={50} />
       <Title>Oops, page not found :c</Title>
       <Description>
         Go back to the homepage and navigate to the page you were trying to
